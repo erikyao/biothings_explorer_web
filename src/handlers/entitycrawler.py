aaf6059fd4bcc86bf03c989ad1cc41054c90b505
kevinxin90 <kevinxin@scripps.edu>
2018-04-30 10:41:42 -0700

    change the workflow using Asyncio

--- src/handlers/entitycrawler.py
+++ src/handlers/entitycrawler.py
@@ -1,24 +1,28 @@
-import requests
-import grequests
+#import requests
+import asyncio
+import aiohttp
+import time
 from collections import defaultdict
 import json
 from timeit import default_timer as timer
-import requests_cache
-from joblib import Parallel, delayed
-import multiprocessing
+#import requests_cache
+#from joblib import Parallel, delayed
+#import multiprocessing
 
 from biothings_explorer import BioThingsExplorer
 from biothings_explorer.jsonld_processor import JSONLDHelper
 from biothings_explorer.utils import property_uri_2_prefix_dict
+from biothings_explorer.output_organizer import OutputOrganizor
 from .basehandler import BaseHandler
 
-requests_cache.install_cache('biothings_cache', backend='sqlite', expire_after=36000)
+#requests_cache.install_cache('biothings_cache', backend='sqlite', expire_after=36000)
 bt_explorer = BioThingsExplorer()
 jh = JSONLDHelper()
+oo = OutputOrganizor()
 
 def find_endpoint(input_type):
     """
-    This function takes input_type as input, and return all endpoints which can ingest the input_type
+    This function takes input_type, e.g. ncbigene as input, and return all endpoints which can ingest the input_type
 
     Return
     ======
@@ -26,15 +30,21 @@ def find_endpoint(input_type):
     """
     return list(bt_explorer.api_map.successors(input_type))
 
-def get_json_helper(_endpoint, input_type, input_value):
-    params = bt_explorer.apiCallHandler.call_api({input_type: input_value}, _endpoint)
-    response = requests.get(params[0], params=params[1], headers={'Accept': 'application/json'})
-    if response.status_code == 200:
-        return bt_explorer.apiCallHandler.preprocess_json_doc(response.json(), _endpoint)
-    else:
-        return {}
+async def get_json_helper(_endpoint, input_type, input_value):
+    api_call_params = bt_explorer.apiCallHandler.call_api({input_type: input_value}, _endpoint)
+    try:
+        print('Start making API calls to {}'.format(_endpoint))
+        response = await aiohttp.request('GET', api_call_params[0], params=api_call_params[1], headers={'Accept': 'application/json'})
+    except:
+        return {'endpoint': _endpoint, 'data': {}}
+    #response = requests.get(params[0], params=params[1], headers={'Accept': 'application/json'})
+    #if response.status_code == 200:
+    print('Start get json output from {}'.format(_endpoint))
+    json_response = await response.json()
+    data = bt_explorer.apiCallHandler.preprocess_json_doc(json_response, _endpoint)
+    return {'endpoint': _endpoint, 'data': data}
 
-def get_json(endpoints, input_type, input_value):
+async def get_json(endpoints, input_type, input_value):
     """
     Given endpoint_name as a list, input_type and input_value,
     Make API calls for each pair of (endpoint, input_type, input_value),
@@ -46,6 +56,11 @@ def get_json(endpoints, input_type, input_value):
     """
     # this code transform prefix to URI
     input_type = bt_explorer.registry.prefix2uri(input_type)
+    start = time.time()
+    tasks = [asyncio.ensure_future(get_json_helper(_endpoint, input_type, input_value)) for _endpoint in endpoints]
+    results = await asyncio.wait(tasks)
+    print("Process took: {:.2f} seconds".format(time.time() - start))
+    return results
     """
     # construct API calls for each endpoint, organize them into a list
     api_call_params = []
@@ -54,11 +69,15 @@ def get_json(endpoints, input_type, input_value):
     # use grequest to make asynchronized API calls
     rs = (grequests.get(u, params=v, headers={'Accept': 'application/json'}) for (u,v) in api_call_params)
     # get JSON output
-    responses = [bt_explorer.apiCallHandler.preprocess_json_doc(api_call_response.json(), endpoint_name) if api_call_response.status_code == 200 else {} for api_call_response in grequests.map(rs)]
+    responses = [bt_explorer.apiCallHandler.preprocess_json_doc(api_call_response.json(), endpoint_name)
+    if api_call_response.status_code == 200 else {} for api_call_response in grequests.map(rs)]
     """
+    """
+    # multiprocessing solution
     num_cores = multiprocessing.cpu_count()
     results = Parallel(n_jobs=num_cores)(delayed(get_json_helper)(_endpoint, input_type, input_value) for _endpoint in endpoints)
     return list(zip(endpoints, results))
+    """
 
 def uri2curie(URI):
     """
@@ -95,7 +114,7 @@ def extractnquads(nquads):
     # Loop through each nquad
     pairs = []
     for _nquad in nquads["@default"]:
-         if not _nquad['object']['value'].startswith('_:'):
+        if not _nquad['object']['value'].startswith('_:'):
             _object = _nquad['object']['value']
             _predicate = _nquad['predicate']['value']
             curie = uri2curie(_object)
@@ -115,35 +134,56 @@ def exploreinput(input_type, input_value):
     ======
     List of (CURIE, RELATION, Endpoint, API)
     """
+    ###################################################################
+    """
+    This part use asyncio libary to asynchronously extract
+    all the JSON outputs from individual API calls
+    """
     endpoints = find_endpoint(input_type)
-    json_docs = get_json(endpoints, input_type, input_value)
+    ioloop = asyncio.get_event_loop()
+    done, _ = ioloop.run_until_complete(get_json(endpoints, input_type, input_value))
+    json_docs = []
+    for fut in done:
+        json_docs.append(fut.result())
+    ###################################################################
+    """
+    This part add JSON-LD context file to individual JSON document
+    """
     jsonld_docs = []
     for json_doc in json_docs:
-        endpoint_name = json_doc[0]
+        endpoint_name = json_doc['endpoint']
         if 'jsonld_context' in bt_explorer.registry.endpoint_info[endpoint_name]:
             jsonld_context_path = bt_explorer.registry.endpoint_info[endpoint_name]['jsonld_context']
-            jsonld_docs.append(jh.json2jsonld(json_doc[1], jsonld_context_path))
+            jsonld_docs.append(jh.json2jsonld(json_doc['data'], jsonld_context_path))
         else:
             jsonld_docs.append(None)
+    ###################################################################
+    """
+    This part convert JSON-LD document to Nquads format
+    And extract and organize the data
+    """
+    # rearrange the endpoints, because Asyncio will reorder the sequence
+    endpoints = [_json['endpoint'] for _json in json_docs]
+    # convert a list of jsonld documents to nquads documents
     nquads_list = jh.jsonld2nquads(jsonld_docs)
     outputs = defaultdict(list)
-    for endpoint, nquads in list(zip(endpoints,nquads_list)):
+    for endpoint, nquads in list(zip(endpoints, nquads_list)):
         # get all possible associations of the endpoint
         association_list = bt_explorer.registry.endpoint_info[endpoint]['associations']
         if "@default" in nquads:
             _output = jh.fetch_properties_by_association_in_nquads(nquads["@default"], association_list)
             for _assoc, _objects in _output.items():
                 for _object in _objects:
-                    reorganized_data = {'endpoint': endpoint, 'api': bt_explorer.registry.endpoint_info[endpoint]['api'], 'predicate': _assoc.replace('http://biothings.io/explorer/vocab/objects/', '')}
-                    for _property, _uris in _object.items():
-                        if len(_uris) == 1:
-                            reorganized_data.update({property_uri_2_prefix_dict[_property]: uri2curie(_uris[0])})
-                        else:
-                            curies = [uri2curie(_uri) for _uri in _uris]
-                            reorganized_data.update({property_uri_2_prefix_dict[_property]: curies})
-                    object_id_prefix = reorganized_data['object.id'].split(':')[0]
+                    reorganized_data = {'endpoint': endpoint, 'api': bt_explorer.registry.endpoint_info[endpoint]['api'],
+                                        'predicate': _assoc.replace('http://biothings.io/explorer/vocab/objects/', '')}
+                    reorganized_data.update(oo.nquads2dict(_object))
+                    object_id_prefix = reorganized_data['target']['id'].split(':')[0]
+                    reorganized_data.update({'prefix': object_id_prefix})
                     object_semantic_type = bt_explorer.registry.prefix2semantictype(object_id_prefix)
                     outputs[object_semantic_type].append(reorganized_data)
+    ###################################################################
+
+    """
     property_summary = defaultdict(set)
     semantic_type_summary = defaultdict(set)
     summary = {}
@@ -151,8 +191,24 @@ def exploreinput(input_type, input_value):
         summary[semantic_type] = defaultdict(set)
         for _doc in pair:
             for _property, _value in _doc.items():
-                if _property == 'object.id':
-                    summary[semantic_type][_property].add(_value.split(':')[0])
+                if _property == 'prefix':
+                    summary[semantic_type][_property].add(_value)
+                elif _property in ['relation.label', 'relation.id', 'publication', 'evidence', 'object.taxonomy']:
+                    if type(_value) != list:
+                        if ':' in _value:
+                            _prefix = _value.split(':')[0]
+                            _value_no_prefix = _value[len(_prefix)+1:]
+                            summary[semantic_type][_prefix].add(_value_no_prefix)
+                        else:
+                            summary[semantic_type][_property].add(_value)
+                    else:
+                        for _single_value in _value:
+                            if ':' in _single_value:
+                                _prefix = _single_value.split(':')[0]
+                                _value_no_prefix = _single_value[len(_prefix)+1:]
+                                summary[semantic_type][_prefix].add(_value_no_prefix)
+                            else:
+                                summary[semantic_type][_property].add(_single_value)
                 elif _property not in ['object.id-secondary', 'object.label']:
                     if type(_value) != list:
                         summary[semantic_type][_property].add(_value)
@@ -162,7 +218,8 @@ def exploreinput(input_type, input_value):
     for k,v in summary.items():
         for _k, _v in v.items():
             summary[k][_k] = list(_v)
-    return {'linkedData': outputs, 'summary': summary}
+    """
+    return {'linkedData': outputs}
     """
         if _output:
             for _pair in _output:
@@ -189,7 +246,7 @@ def exploreinput(input_type, input_value):
 class Crawler(BaseHandler):
     """
     This function serves as one BioThings Explorer API endpoint
-    Given an input_type and input_value, 
+    Given an input_type and input_value,
     return all biological entities(type & value) which could be linked to this entity
 
     Params
